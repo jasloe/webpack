920215d3e51e8b797393cb7c9024123caff1de66
Jason Loeffler <j@minorscience.com>
2019-09-03 10:07:45 -0400

    boilerplate

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,14 @@
 // import scss
 import "../scss/main.scss";
 
+console.log(a);
+
 // ES6 example code
 if (true) {
  let a = 40;
  console.log(a); //40
 }
-console.log(a); // undefined
+
+a = 4;
+const a;
+
